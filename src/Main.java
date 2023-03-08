437d8449e9feec44ef4eeb90f19f30757c8f5283
AnamariaMantescu <mantescuanamaria20@stud.ase.ro>
2023-03-08 16:15:18 +0200

    primul commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//orice
 public class Main {
 
     public static void main(String[] args) {
